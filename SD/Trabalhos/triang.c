bde15dfad8d7ab87c0b9fdb3c59315f2aaf33dc4
Marcelo EK <31110991+marceloek@users.noreply.github.com>
2017-10-05 04:05:43 -0300

    Update triang.c

--- SD/Trabalhos/triang.c
+++ SD/Trabalhos/triang.c
@@ -1,38 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int verifica(int xA, int xB, int xC, int yA, int yB, int yC, int areatotal, int i, int j){
-	int area1, area2, area3;
-	area1=(xA*j + yA*xC + i*yC - (j*xC + xA*yC + yA*i));
-	if(area1<0)
-		area1=area1*(-1);
-	area2=(i*yB + j*xC + xB*yC - (yB*xC + i*yC + j*xB));
-	if(area2<0)
-		area2=area2*(-1);	
-	area3=(xA*yB + yA*i + xB*j - (yB*i + xA*j + yA*xB));	
-	if(area3<0)
-		area3=area3*(-1);
-	if((area1+area2+area3)<=areatotal)
+int conf(int xA, int xB, int xC, int yA, int yB, int yC, int areaABC, int i, int j){
+	int areaAC, areaBC, areaAB;
+	areaAC=(xA*j + yA*xC + i*yC - (j*xC + xA*yC + yA*i));
+	if(areaAC<0)
+		areaAC=areaAC*(-1);
+	areaBC=(i*yB + j*xC + xB*yC - (yB*xC + i*yC + j*xB));
+	if(areaBC<0)
+		areaBC=areaBC*(-1);	
+	areaAB=(xA*yB + yA*i + xB*j - (yB*i + xA*j + yA*xB));	
+	if(areaAB<0)
+		areaAB=areaAB*(-1);
+	if((areaAC+areaBC+areaAB)<=areaABC)
 		return 1;
 	else
 		return 0;
 }
 
 int main(){
-	int xA, xB, xC, yA, yB, yC, areatotal, i, j;
-	printf("Digite o valor da coordenada de A: ");
+	int xA, xB, xC, yA, yB, yC, areaABC, i, j;
+	printf("Digite as coordendas da posicao A: ");
 	scanf("%d %d", &xA, &yA);
-	printf("Digite o valor da coordenada de B: ");
+	printf("Digite as coordendas da posicao B: ");
 	scanf("%d %d", &xB, &yB);
-	printf("Digite o valor da coordenada de C: ");
+	printf("Digite as coordendas da posicao C: ");
 	scanf("%d %d", &xC, &yC);
-	areatotal=(xA*yB + yA*xC + xB*yC - (yB*xC + xA*yC + yA*xB));
-	if(areatotal<0)
-		areatotal=areatotal*(-1);
+	areaABC=(xA*yB + yA*xC + xB*yC - (yB*xC + xA*yC + yA*xB));
+	if(areaABC<0)
+		areaABC=areaABC*(-1);
 	system("clear");
 	for(i=0;i<40;i++){
 		for(j=0;j<40;j++){
-			if(verifica(xA, xB, xC, yA, yB, yC, areatotal, i, j)==1)
+			if(conf(xA, xB, xC, yA, yB, yC, areaABC, i, j)==1)
 				printf("7");
 			else
 				printf(".");
