957d4edb78d43976c2ada040dd14166a1b14f3f1
Marcelo EK <31110991+marceloek@users.noreply.github.com>
2018-03-15 17:11:50 -0300

    Add files via upload

--- ORG/decforbin.c
+++ ORG/decforbin.c
@@ -19,36 +19,45 @@ void fracaoParaBinario(double fracao, int rnum[52]){
 
 int main(){
 	int Bias = 1023, rnum[52], E=-1, exp, b;
-	for(int b=0;b<52;b++)
-		rnum[b]=0;
 	char exp1[11];
 	double tnum, aux;
-	printf("Digite um numero: ");
+	
+	//colocando valores '0' para todas as posicoes da parte fracionaria
+	for(int b=0;b<52;b++) 
+		rnum[b]=0;
+		
+	printf("Digite um numero: "); //digitar o valor de entrada
 	scanf("%lf", &tnum);
+	
 	aux = tnum;
-	if(aux<0)
+	
+	if(aux<0) //caso o valor seja negativo
 		aux *=-1;
-	for(int a=0;aux>1;a++){
+		
+	//divisao sucessiva maior que 1 para encontrar o numero de bits
+	for(int a=0;aux>1;a++){ 
 		aux /=2;
 		E++;
 	}
 	aux*=2;
-	printf("%lf", aux);
-	fracaoParaBinario(aux, rnum);
-	exp = E + Bias;
-	itoa(exp, exp1, 2); //converte para base 2
 	
+	fracaoParaBinario(aux, rnum); //convertendo parte fracionaria
 	
-	if(tnum<0)	//printando S
-		printf("| 1 | ");
+	exp = E + Bias; //somando bits da base 2 com os bits do double precision
+	
+	itoa(exp, exp1, 2); //conversao para base 2
+	
+	if(tnum<0)	//printando S(1)	obs: (n)=bits utilizados
+		printf("\n| 1 | ");
 	else
 		printf("| 0 | ");
-	printf("%s | ", exp1); //printando exp
+	printf("%s | ", exp1); //printando exp(11)
 	
 	for(b=0;b<51;b++){
-		printf("%d", rnum[b]);
+		printf("%d", rnum[b]); //printando frac(51)
 	}
 	if(b==51)
-			printf("%d |", rnum[b]);	
+			printf("%d |", rnum[b]); //printando frac(52)
+	printf("\nS(1)	exp(11)				frac(52)");
 	return 0;
 }
