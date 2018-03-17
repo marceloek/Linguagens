1374426d065c4c3732fb01e371e61fcf9cb0944e
Marcelo EK <31110991+marceloek@users.noreply.github.com>
2018-03-17 06:19:21 -0300

    Add files via upload

--- ORG/decforbin.c
+++ ORG/decforbin.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <conio.h>
 
-void fracaoParaBinario(double fracao, int rnum[52]){
+void mantissa(double fracao, int rnum[52]){
 	if(fracao - (int)fracao != 0){
 		int i=0;
 		fracao = fracao - (int)fracao;
@@ -17,47 +16,74 @@ void fracaoParaBinario(double fracao, int rnum[52]){
 	}
 }
 
+void fexp(int aux, int exp1[11]){
+    int i=10;
+    while(aux>=2){
+        exp1[i]=aux%2;
+        aux /=2;
+        i--;
+    }
+    exp1[i--]=aux;
+}
+
 int main(){
-	int Bias = 1023, rnum[52], E=-1, exp, b;
-	char exp1[11];
+	int Bias = 1023, rnum[52], E=0, exp01, b, exp1[11];
 	double tnum, aux;
-	
-	//colocando valores '0' para todas as posicoes da parte fracionaria
-	for(int b=0;b<52;b++) 
+
+	//colocando valores '0' para todas as posicoes da mantissa e exp
+	for(int b=0;b<52;b++)
 		rnum[b]=0;
-		
+	for(int b=0;b<11;b++)
+		exp1[b]=0;
+
 	printf("Digite um numero: "); //digitando o valor de entrada
 	scanf("%lf", &tnum);
-	
+
 	aux = tnum;
-	
+
 	if(aux<0) //caso o valor seja negativo
 		aux *=-1;
-		
+
 	//divisao sucessiva maior que 1 para encontrar o numero de bits
-	for(int a=0;aux>1;a++){ 
-		aux /=2;
-		E++;
+  if(aux>1){
+      while(aux>1){
+					E++;
+        	aux /=2;
+					if(aux/2<1)
+						break;
+	    }
 	}
-	aux*=2;
-	
-	fracaoParaBinario(aux, rnum); //convertendo parte fracionaria
-	
-	exp = E + Bias; //somando bits da base 2 com os bits do double precision
-	
-	itoa(exp, exp1, 2); //conversao para base 2
-	
+
+	//multiplicacao sucessiva menor que 1 para encontrar o numero de bits
+	if(aux<1){
+    	while(aux<1){
+					E--;
+        	aux *=2;
+					if(aux*2>1)
+						break;
+	    }
+	}
+
+	mantissa(aux, rnum); //convertendo parte fracionaria
+
+	exp01 = E + Bias; //somando bits da base 2 com os bits do double precision
+
+	fexp(exp01, exp1); //calculando exp
+
 	if(tnum<0) //printando S(1)	obs: (n)=bits utilizados
-		printf("\n| 1 | ");
+		printf("| 1 | ");
 	else
 		printf("| 0 | ");
-	printf("%s | ", exp1); //printando exp(11)
-	
+
+    for(int n=0;n<11;n++)
+        printf("%d", exp1[n]); //printando exp(11)
+    printf(" |");
+
 	for(b=0;b<51;b++){
 		printf("%d", rnum[b]); //printando frac(51)
 	}
 	if(b==51)
 			printf("%d |", rnum[b]); //printando frac(52)
-	printf("\nS(1)	exp(11)				frac(52)");
+	printf("\nS(1)	exp(11)				frac(52)\n");
 	return 0;
 }
