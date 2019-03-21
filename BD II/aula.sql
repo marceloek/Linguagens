4be796134dde2df6ff711dd4a1c70a611d8fa6ac
Marcelo EK <31110991+marceloek@users.noreply.github.com>
2019-03-21 09:21:00 -0300

    Add files via upload

---
+++ BD II/aula.sql
@@ -0,0 +1,32 @@
+sudo -u postgres psql postgres
+
+create table cliente(numero int primary key, cpf int, nome varchar(50));
+
+begin;
+fsafsad;
+end;
+
+begin;
+insert into cliente values (213213, 551234, 'asdds');
+
+insert into cliente values (521532, 242344, 'hhrgh');
+
+insert into cliente values (421412, 214152, 'hgrhh');
+
+end;
+
+begin;
+
+insert into cliente values (666666, 214152, 'hgrhh');
+
+end;
+
+sudo -u postgres psql postgres
+
+create table cliente2(numero int, cpf int, nome varchar(50));
+
+begin;
+
+insert into cliente2 values (6, 214152, 'hgrhh');
+
+end;
