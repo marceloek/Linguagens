08c7770761adcfca8f3dfecb608e7d53041fd10f
Marcelo EK <31110991+marceloek@users.noreply.github.com>
2019-04-06 01:35:24 -0300

    Add files via upload

--- ORG/formaiormenor.asm
+++ ORG/formaiormenor.asm
@@ -1,15 +1,15 @@
             .data
-vector_A:	.word	  31, 5, -2, 5, -10, 11, 30, 7, 0, -6
+vector_A:	.word	  31, 5, -2, 5, -15, 11, 30, 7, 0, -6
 
 	.text
 main:
-	la $a0, vector_A   		# a0 armazena vetor[10]
-    addi $a1, $zero, 10		# a1 armazena tamanho do vetor
-    addi	$s0, $zero, -99			# a = -99
-    addi	$s1, $zero, 99			# b = 99
-    add	$s2, $zero, $zero			# z = 0
+	la $a0, vector_A   			# a0 armazena vetor[10]
+    addi $a1, $zero, 10			# a1 armazena tamanho do vetor
+    addi	$s0, $zero, -999	# a = -99
+    addi	$s1, $zero, 999		# b = 99
+    add	$s2, $zero, $zero		# z = 0
 
-teste: slti $t0, $s2, 10		# testa condiçao de final de for
+teste: slt $t0, $s2, $a1	# testa condiçao de final de for
 	beq  $t0, $zero, fim	# termina for
 	sll  $t1, $s2, 2		# multilica x por 4
 	add  $t3, $a0, $t1		# calcula posiçao vetor[x]
@@ -20,7 +20,7 @@ teste: slti $t0, $s2, 10		# testa condiçao de final de for
 	add $s0, $t2, $zero
 maior:
 	slt $t0, $t2, $s1		# verifica se é maior que vetor[x]
-	beq $t0, $zero, menor	#
+	beq $t0, $zero, menor
 	add $s1, $t2, $zero
 menor:
 	addi $s2, $s2, 1		# z++
