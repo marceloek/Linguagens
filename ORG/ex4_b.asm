191aa95a666a2226f64d4a3964b723af8d1166e4
Marcelo EK <31110991+marceloek@users.noreply.github.com>
2019-04-22 22:55:05 -0300

    Add files via upload

--- ORG/ex4_b.asm
+++ ORG/ex4_b.asm
@@ -8,15 +8,17 @@ main:
     addi $s2, $zero, 100    # c
     addi $s3, $zero, 10	    # d
 
-    div $a0, $s0, $s2
-    mul $a1, $s1, $s3
-    add $a2, $a0, $a1
-    subi $a2, $a2, 50
+    div $a0, $s0, $s2       # x0 = a / c
+    mul $a1, $s1, $s3       # x1 = b * d
+    add $a2, $a0, $a1       # x = x0 + x1
+    subi $a2, $a2, 50       # x = x - 50
 
-    slt $t0, $s1, $a3	    # c >= 101
+    slt $t0, $s2, $a3	    # c >= 101
     beq $t0, $zero, if1
-
-    mul $s2, $s2, 5
-
+    mul $s2, $s2, 5         # c *= 5
+    j fim
 if1:
-    add $s2, $s2, $zero
+    add $s2, $zero, $zero     # c = 0
+    j fim
+
+fim:
