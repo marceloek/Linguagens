014b216c016479e20c47e78da31fc099aacf8c32
Marcelo EK <31110991+marceloek@users.noreply.github.com>
2019-05-08 17:39:17 -0300

    Add files via upload

---
+++ BD II/Ex3_Prova(Marcelo Knob).sql
@@ -0,0 +1,20 @@
+-- Acadêmico: Marcelo Elias Knob
+
+CREATE OR REPLACE FUNCTION Salario() RETURNS TRIGGER AS $$
+DECLARE
+	maxi decimal(10,2);
+	mini decimal(10,2);
+BEGIN
+	select max(salario) into maxi from empregado;
+	select min(salario) into mini from empregado;
+	if NEW.salario > maxi then
+		RAISE EXCEPTION '% esta acima do limite máximo permitido', NEW.salario;
+	end if;
+	if NEW.salario < mini then
+		RAISE EXCEPTION '% esta abaixo do limite minimo permitido', NEW.salario;
+	end if;
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER insere_empregado before INSERT ON empregado FOR EACH ROW EXECUTE PROCEDURE Salario();
