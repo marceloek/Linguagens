1fbcfad21b5a442ef8ab526e3f712f7c82d20afa
Marcelo EK <31110991+marceloek@users.noreply.github.com>
2019-09-24 09:58:39 -0300

    Add files via upload

--- SO/philosopher.c
+++ SO/philosopher.c
@@ -1,132 +1,87 @@
 // gcc -o test1 philosopher.c -lpthread
-
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <semaphore.h>
-#include <fcntl.h>
 
-void *mythread(void *data);
-
-#define N 5                     // numero de philosopher
+#define N 5 // numero de philosopher
 #define thinking 0
 #define hungry 1
 #define eating 2
+#define left (i + N - 1) % N //agarrar garfo da esquerda
+#define right (i + 1) % N    //agarrar garfo da direita
 
-typedef int semaphore;
-int state[N];                   // estados dos philosophers (comendo, pensando e com fome)
+void *mythread(void *data);
+void take_forks(int);
+void put_forks(int);
+void test(int);
 
-semaphore mutex = 1;
-semaphore phil_sem[N];          // mutex de cada philosopher
+int nfilosofo[N] = {0, 1, 2, 3, 4};
+int state[N] = {0, 0, 0, 0, 0}; // estados dos philosophers (comendo, pensando e com fome)
+sem_t phil_sem[N];              // mutex de cada philosopher
+sem_t mutex;
 
-
-int right(int i)
+void *mythread(void *num)
 {
-    int right_int = (i + 1) % N;
-    printf("(philosopher right [%d])\n", right_int);
-    return right_int;
+    while (1)
+    {
+        int *id = num;
+        sleep(1);
+        take_forks(*id);
+        sleep(1);
+        put_forks(*id);
+    }
 }
 
-int left(int i)
+void take_forks(int i)
 {
-    int left_int = (i + N - 1) % N;
-    printf("(philosopher left [%d])\n", right_int);
-    return left_int;
+    sem_wait(&mutex);
+    state[i] = hungry;
+    printf("Filosofo %d tem fome.\n", i);
+    test(i);
+    sem_post(&mutex);
+    sem_wait(&phil_sem[i]); //down semaphore philosopher
+    sleep(1);
 }
 
 void put_forks(int i)
 {
-    int left_int, right_int;
     sem_wait(&mutex);
-
     state[i] = thinking;
-
-    right_int = right(i);
-    left_int = left(i);
-    test(left_int);
-    test(right_int);
-
+    printf("Filosofo %d deixou os garfos %d e %d.\n", i, left, right);
+    printf("Filosofo %d esta a pensar.\n", i);
+    test(left);
+    test(right);
     sem_post(&mutex);
 }
 
 void test(int i)
 {
-    int right_int, left_int;
-    right_int = right(i);
-    left_int = left(i);
-    if (state[i] == hungry && state[left_int] != eating && state[right_int] != eating)
+    if (state[i] == hungry && state[left] != eating && state[right] != eating)
     {
-        state[i] = hungry;
-        sem_wait(&phil_sem[i]);
+        state[2] = hungry;
+        sleep(1);
+        printf("Filosofo %d agarrou os garfos %d e %d.\n", i, left, right);
+        printf("Filosofo %d esta a comer.\n", i);
+        sem_post(&phil_sem[i]);
     }
-}
-
-
-void take_forks(int i)
-{
-    printf("take forks 1, philo [%d]\n", i);
-    sem_wait(&mutex);
-    printf("take forks 2, philo [%d]\n", i);
-
-    state[i] = hungry;
-    test(i);
-
-    sem_post(&mutex);
-    printf("take forks 3, philo [%d]\n", i);
-
-    sem_wait(&phil_sem[i]); //down semaphore philosopher
-    printf("take forks 4, philo [%d]\n", i);
 }
 
 int main(void)
 {
     int i = 0;
-
-    for (i = 0; i < N; i++)
-    {
-        state[i] = 0;
-        phil_sem[i] = 0;
-    }
-
     pthread_t philosopher[N];
-
+    sem_init(&mutex, 0, 1); // inicializa o semáforo com valor 1
+    for (i = 0; i < N; i++)
+        sem_init(&phil_sem[i], 0, 0); // inicializa o semáforo com valor 1
     for (i = 0; i < N; i++)
     {
         int *j = malloc(sizeof(int));
         *j = i;
-        pthread_create(&philosopher[i], NULL, mythread, (void *)j);
+        pthread_create(&philosopher[i], NULL, mythread, &nfilosofo[i]);
+        printf("Filosofo %d esta a pensar.\n", i);
     }
-    /* We will now wait for each thread to<\n>
-    * terminate */
-
     for (i = 0; i < N; i++)
-    {
         pthread_join(philosopher[i], NULL);
-        printf("Thread id %ld returned\n", philosopher[i]);
-    }
-
     return (1);
-}
-
-void *mythread(void *data)
-{
-    int id;
-    id = *((int *)data);
-    free((int *)data);
-
-    while (1)
-    {
-        printf("filosofo [%d] esta pensando...\n\n", id);
-        sleep(2);
-
-        take_forks(id);
-
-        printf("filosofo [%d] esta comendo...\n\n", id);
-        sleep(3);
-
-        put_forks(id);
-    }
-
-    pthread_exit(NULL);
 }
