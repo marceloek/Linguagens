d44f719e396f671b9b73c4a424c1b91da558a205
Marcelo EK <31110991+marceloek@users.noreply.github.com>
2019-09-30 20:26:23 -0300

    Add files via upload

--- GC/main.cpp
+++ GC/main.cpp
@@ -1,135 +1,209 @@
 /**
- * Basic usege of OpenGL and GLFW. 
+ * This demo shows how to periodically call an update() function to create
+ * animations over time. In this case, the animation is to rotate a red square.
+ *
+ * Author: Fernando Bevilacqua <fernando.bevilacqua@uffs.edu.br>
+ * License: MIT
  */
 
-#include <iostream>
 #include <GL/glut.h>
 #include <GL/gl.h>
-#include <GLFW/glfw3.h>
+#include <iostream>
 
-#define WIDTH 500
-#define HEIGHT 500
+// Constants to define the width/height of the window
+const int WIDTH = 800;
+const int HEIGHT = 800;
 
-float x1 = -0.99;
+// Define how many frames per seconds we want our
+// applications to run.
+const unsigned int FRAMES_PER_SECOND = 60;
+const unsigned int UPDATE_INTERVAL_MS = 1000 / FRAMES_PER_SECOND;
 
-float x2 = 0.1;
-
+// Control the angle to rotate the red square
+float angle = 0;
+float x1 = -0.3;
+float x2 = -0.9;
 float sentido = 1;
 
-GLfloat rx = 0;
-
-int main()
+void renderCoordinateAxis()
 {
-	GLFWwindow *window;
-	int width, height;
+	// X axis - green color
+	glColor3f(0, 1, 0);
+	glBegin(GL_LINES);
+		// Left side, negative X
+		glVertex2f(-10.0, 0.0);
+		glVertex2f(0.0, 0.0);
 
-	if (!glfwInit())
-	{
-		std::cout << "Problem to initialize GLFW" << std::endl;
-		exit(1);
+		// Right side, positive X
+		glVertex2f(0.0, 0.0);
+		glVertex2f(10.0, 0.0);
+	glEnd();
+
+	// Y axis - blue color
+	glColor3f(0, 0, 1);
+	glBegin(GL_LINES);
+		// Top side, positive Y
+		glVertex2f(0.0, 0.0);
+		glVertex2f(0.0, 10.0);
+
+		// Bottom side, negative Y
+		glVertex2f(0.0, 0.0);
+		glVertex2f(0.0, -10.0);
+	glEnd();
+}
+
+void display()
+{
+	// Clear the screen painting it with the white color
+	glClearColor(1, 1, 1, 0);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	// Inform we want to make changes to the modelview matrix, starting
+	// with no transformation at all.
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	// Move the camera away from the origin along the Z axis by 10 pixels.
+	glTranslatef(0, 0, -10.0f);
+
+	// Render the X and Y axis to guide ourselves.
+	renderCoordinateAxis();
+
+	// Rotate the red square by "angle" degrees.
+	glRotatef(angle, 0.0f, 0.0f, 1.0f);
+	
+	glRotatef(angle, 0.0f, 1.0f, 0.0f);
+
+	// Render a red square
+	//        R  G  B
+	glColor3f(1, 0, 0);
+	//        x1    y1    x2     y2
+	//glRectf(-1.0f, 1.0f, 1.0f, -1.0f);
+	glScalef(3.0f,3.0f,3.0f);
+
+	glBegin(GL_LINES);
+	// green
+	glColor3f(0.0f, 1.0f, 0.0f);
+
+	// linha vertical
+	glVertex3f(x1, -0.2f, 0.f);
+	glVertex3f(x1, 0.2f, 0.f);
+
+	// linha horizontal
+	glVertex3f(x1, 0.2f, 0.f);
+	glVertex3f(x1 + 0.4, 0.2f, 0.f);
+
+	// linha horizontal
+	glVertex3f(x1, -0.2f, 0.f);
+	glVertex3f(x1 + 0.4, -0.2f, 0.f);
+
+	// linha vertical
+	glVertex3f(x1 + 0.4, -0.2f, 0.f);
+	glVertex3f(x1 + 0.4, 0.2f, 0.f);
+
+	// red
+	glColor3f(1.0f, 0.0f, 0.0f);
+
+	// linha vertical
+	glVertex3f(x1 + 0.6, 0.0f, 0.3f);
+	glVertex3f(x1 + 0.6, 0.4f, 0.3f);
+
+	// linha vertical
+	glVertex3f(x1 + 0.2, 0.4f, 0.3f);
+	glVertex3f(x1 + 0.2, 0.0f, 0.3f);
+
+	// linha horizontal
+	glVertex3f(x1 + 0.6, 0.0f, 0.3f);
+	glVertex3f(x1 + 0.2, 0.0f, 0.3f);
+
+	// linha horizontal
+	glVertex3f(x1 + 0.6, 0.4f, 0.3f);
+	glVertex3f(x1 + 0.2, 0.4f, 0.3f);
+
+	// blue
+	glColor3f(0.0f, 0.0f, 1.0f);
+
+	// linha diagonal
+	glVertex3f(x1 + 0.6, 0.0f, 0.3f);
+	glVertex3f(x1 + 0.4, -0.2f, 0.f);
+
+	// linha diagonal
+	glVertex3f(x1 + 0.6, 0.4f, 0.3f);
+	glVertex3f(x1 + 0.4, 0.2f, 0.f);
+
+	// linha diagonal
+	glVertex3f(x1 + 0.2, 0.0f, 0.3f);
+	glVertex3f(x1, -0.2f, 0.f);
+
+	// linha diagonal
+	glVertex3f(x1 + 0.2, 0.4f, 0.3f);
+	glVertex3f(x1, 0.2f, 0.f);
+
+	glEnd();
+
+	// Start the rendering on a new frame
+	glutSwapBuffers();
+}
+
+void update(int value)
+{
+	// Update the angle of rotation
+	angle += 3;
+
+	// Request a new frame rendering
+	glutPostRedisplay();
+
+	// Re-schedule the update() function to be called after a few
+	// milliseconds again.
+	glutTimerFunc(UPDATE_INTERVAL_MS, update, 0);
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+	if (key == 27) {
+		// ESC key
+		exit(0);
 	}
+}
 
-	window = glfwCreateWindow(WIDTH, HEIGHT, "Hello world, GLFW!", NULL, NULL);
+void mouse(int button, int state, int x, int y)
+{
+	std::cout << "Mouse pressed: button=" << button << ", state=" << state << ", x=" << x << " y=" << y << std::endl;
+}
 
-	if (!window)
-	{
-		std::cout << "Problem to create GLFW window" << std::endl;
-		glfwTerminate();
-		exit(1);
-	}
+void initView()
+{
+	// We want a window to the world that is a rectangle starting at (0,0)
+	// and ending at (WIDTH, HEIGHT).
+	glViewport(0, 0, (GLsizei)WIDTH, (GLsizei)HEIGHT);
+	
+	// Adjust the global projection (camera)
+	glMatrixMode(GL_PROJECTION);
+	
+	// Reset any existing projection settings and adjust the field-of-view (FOV)
+	glLoadIdentity();
+	glFrustum(-1.0, 1.0, -1.0, 1.0, 1.5, 200.0);
+	
+	// From now on, every transformation is to be applied on each object, e.g. modelview.
+	glMatrixMode(GL_MODELVIEW);
+}
 
-	glfwMakeContextCurrent(window);
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowSize(WIDTH, HEIGHT);
+	glutCreateWindow("Transformation - Simple");
+	glutDisplayFunc(display);
+	glutKeyboardFunc(keyboard);
+	glutMouseFunc(mouse);
 
-	while (glfwWindowShouldClose(window) == 0 && glfwGetKey(window, GLFW_KEY_ESCAPE) == 0)
-	{
-		glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	initView();
 
-		glfwGetFramebufferSize(window, &width, &height);
-		glViewport(0, 0, width, height);
+	// Schedule the update() function to be called after a few
+	// milliseconds (calculated as 1000 milliseconds divided by FPS).
+	glutTimerFunc(UPDATE_INTERVAL_MS, update, 0);
 
-		// if (x1 + 0.6 >= 0.99)
-		// {
-		// 	sentido = -1;
-		// 	//x1 = 1;
-		// }
-		// if (x1 <= -0.9)
-		// {
-		// 	sentido = 1;
-		// 	//x1 = -1;
-		// }
-		// x1 = x1 + 0.0002 * sentido;
-
-
-		glBegin(GL_LINES);
-		// green
-		glColor3f(0.0f, 1.0f, 0.0f);
-
-		// linha vertical
-		glVertex3f(x1, -0.1f, 0.f);
-		glVertex3f(x1, 0.3f, 0.f);
-
-		// linha horizontal
-		glVertex3f(x1, 0.3f, 0.f);
-		glVertex3f(x1 + 0.4, 0.3f, 0.f);
-
-		// linha horizontal
-		glVertex3f(x1, -0.1f, 0.f);
-		glVertex3f(x1 + 0.4, -0.1f, 0.f);
-
-		// linha vertical
-		glVertex3f(x1 + 0.4, -0.1f, 0.f);
-		glVertex3f(x1 + 0.4, 0.3f, 0.f);
-
-
-		glScalef(4, 4, 4);
-
-		// red
-		glColor3f(1.0f, 0.0f, 0.0f);
-
-		// // linha vertical
-		glVertex3f(x1 + 0.6, 0.1f, 0.f);
-		glVertex3f(x1 + 0.6, 0.5f, 0.f);
-
-		// // linha vertical
-		glVertex3f(x1 + 0.2, 0.5f, 0.f);
-		glVertex3f(x1 + 0.2, 0.1f, 0.f);
-
-		// // linha horizontal
-		glVertex3f(x1 + 0.6, 0.1f, 0.f);
-		glVertex3f(x1 + 0.2, 0.1f, 0.f);
-
-		// // linha horizontal
-		glVertex3f(x1 + 0.6, 0.5f, 0.f);
-		glVertex3f(x1 + 0.2, 0.5f, 0.f);
-
-		// // blue
-		glColor3f(0.0f, 0.0f, 1.0f);
-
-		// // linha diagonal
-		glVertex3f(x1 + 0.6, 0.1f, 0.f);
-		glVertex3f(x1 + 0.4, -0.1f, 0.f);
-
-		// // linha diagonal
-		glVertex3f(x1 + 0.6, 0.5f, 0.f);
-		glVertex3f(x1 + 0.4, 0.3f, 0.f);
-
-		// // linha diagonal
-		glVertex3f(x1 + 0.2, 0.1f, 0.f);
-		glVertex3f(x1, -0.1f, 0.f);
-
-		// // linha diagonal
-		glVertex3f(x1 + 0.2, 0.5f, 0.f);
-		glVertex3f(x1, 0.3f, 0.f);
-
-		glEnd();
-
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
-
-	glfwDestroyWindow(window);
-	glfwTerminate();
-
+	glutMainLoop();
 	return 0;
 }
