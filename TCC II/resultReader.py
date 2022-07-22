56d1ca0d7a48c6d76823ff9e1ec82c5b3de2966b
Marcelo Knob <31110991+marceloek@users.noreply.github.com>
2022-07-22 17:32:28 -0300

    Add file result creation

--- TCC II/resultReader.py
+++ TCC II/resultReader.py
@@ -1,36 +1,64 @@
 import os
 
-listCalc = 0
-listResult = [[], []]
 
-subsTypes = ['/normal_sub/', '/shared_sub/']
-scenarios = [1, 2, 3, 4]
-messages = [2, 10, 50, 100]
+def main():
+    listCalc = 0
+    listResult = [[], []]
 
-count = 0
+    subsTypes = ['/normal_sub/', '/shared_sub/']
+    scenarios = [1, 2, 3, 4, 5, 6]
+    messages = [2, 10, 50, 100]
 
-for j in subsTypes:
-    for i in scenarios:
-        for k in messages:
-            path = "/home/marceloek/Desktop/result/" + \
-                str(i) + j + str(k) + '/'
+    count = 0
 
-            dirList = os.listdir(path)
+    for j in subsTypes:
+        for i in scenarios:
+            for k in messages:
+                path = "/home/marceloek/Desktop/TCC_II_Experimento/result/" + \
+                    str(i) + j + str(k) + '/'
 
-            for resultFileCSV in dirList:
-                with open(path + resultFileCSV) as result:
-                    lines = result.readlines()
-                    totalLatency = (
-                        float(lines[-1].split(',')[0]) - float(lines[0].split(',')[0])) / 100
+                dirList = os.listdir(path)
+                validFiles = len(dirList)
 
-                    listCalc += totalLatency
+                for resultFileCSV in dirList:
+                    with open(path + resultFileCSV) as result:
+                        lines = result.readlines()
+                        if (len(lines) < 2000):
+                            validFiles -= 1
+                            continue
 
-            listResult[count].append(listCalc / 10)
-            listCalc = 0
-    count += 1
+                        totalLatency = (
+                            float(lines[-1].split(',')[0]) - float(lines[0].split(',')[0]))
 
-for x in range(0, len(scenarios) * len(messages)):
-    print((listResult[0][x] - listResult[1][x]) / 10000, 'ms')
+                        listCalc += totalLatency
 
-# print(listResult[0])
-# print(listResult[1])
+                listResult[count].append(listCalc / validFiles)
+                listCalc = 0
+        count += 1
+
+    count = 0
+    countMessages = 0
+
+    pathResultFile = '/home/marceloek/Desktop/TCC_II_Experimento/result/resultScenarios.txt'
+    f = open(pathResultFile, 'w')
+
+    for x in range(0, len(scenarios) * len(messages)):
+        if((x % 4) == 0):
+            f.write('\nScenario: [' + str(scenarios[count]) + ']\n')
+            count += 1
+            countMessages = 0
+
+        f.write(str(listResult[0][x]) + ',' + str(listResult[1][x]) + '\n')
+        f.write('Number Messages [' + str(messages[countMessages]) + ']: ' +
+                str((listResult[0][x] - listResult[1][x]) / 1000) + ' ms\n')
+
+        countMessages += 1
+
+    f.close()
+
+    f = open(pathResultFile, 'r')
+    print(f.read())
+    f.close()
+
+
+main()
